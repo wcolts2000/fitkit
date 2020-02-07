e1c0162e702b5debd6ece97a0d0db70f6db31e3b
wcolts2000 <wcolts2000@gmail.com>
2020-02-06 19:45:43 -0800

    added get single user test

--- userRoutes/userRouter.test.js
+++ userRoutes/userRouter.test.js
@@ -13,6 +13,11 @@ const loginCreds = {
   email: 'testemail@email.com'
 };
 
+const regUser = async () =>
+  await request(server)
+    .post('/users/register')
+    .send(user);
+
 describe('user auth router', () => {
   beforeEach(async () => {
     await db('users').truncate();
@@ -20,9 +25,7 @@ describe('user auth router', () => {
   describe('POST /users/register & /users/login', () => {
     it('should return 201 status and token when sent a POST to /users/register ', async () => {
       const expected = 201;
-      let res = await request(server)
-        .post('/users/register')
-        .send(user);
+      let res = await regUser();
       const token = res.body.token;
 
       expect(res.status).toEqual(expected);
@@ -31,9 +34,7 @@ describe('user auth router', () => {
     it('should return a 200 status and token when a valid user logs in via POST to /users/login', async () => {
       const expected = 200;
 
-      let res = await request(server)
-        .post('/users/register')
-        .send(user);
+      let res = await regUser();
 
       res = await request(server)
         .post('/users/login')
@@ -44,11 +45,24 @@ describe('user auth router', () => {
       expect(token).not.toBe(undefined);
     });
   });
+  describe('GET /users/:id', () => {
+    it('returns a 401 Unauthorized when no token is provided inj headers', async () => {
+      await regUser();
+      return request(server)
+        .get('/users/1')
+        .expect(401);
+    });
+    it('returns a 200 when a token is provided', async () => {
+      const userObj = await regUser();
+      return request(server)
+        .get('/users/1')
+        .set('Authorization', userObj.body.token)
+        .expect(200);
+    });
+  });
   describe('PUT /users/:id', () => {
     it('returns a 401 Unauthorized when no token is provided in headers', async () => {
-      await request(server)
-        .post('/users/register')
-        .send(user);
+      await regUser();
       return request(server)
         .put('/users/1')
         .send({
@@ -57,9 +71,7 @@ describe('user auth router', () => {
         .expect(401);
     });
     it('returns a 200 OK when a valid token is provided in headers', async () => {
-      const userObj = await request(server)
-        .post('/users/register')
-        .send(user);
+      const userObj = await regUser();
       return request(server)
         .put('/users/1')
         .set('Authorization', userObj.body.token)
@@ -68,9 +80,7 @@ describe('user auth router', () => {
     });
   });
   it('correctly updates the entry passed to it in the req body', async () => {
-    const userObj = await request(server)
-      .post('/users/register')
-      .send(user);
+    const userObj = await regUser();
     let updatedUser = await request(server)
       .put('/users/1')
       .set('Authorization', userObj.body.token)
